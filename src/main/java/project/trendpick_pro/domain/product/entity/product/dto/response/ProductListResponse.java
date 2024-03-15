5960d57699c0845a79b798e4f072074552767494
SoohoLee <hyeri0603@naver.com>
2024-03-16 01:13:10 +0900

    refactor: data 구조 개선

--- src/main/java/project/trendpick_pro/domain/product/entity/product/dto/response/ProductListResponse.java
+++ src/main/java/project/trendpick_pro/domain/product/entity/product/dto/response/ProductListResponse.java
@@ -3,15 +3,12 @@ package project.trendpick_pro.domain.product.entity.product.dto.response;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.AccessLevel;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
-import project.trendpick_pro.domain.product.entity.product.Product;
 
-import java.io.Serializable;
-
-@Data
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class ProductListResponse implements Serializable {
+public class ProductListResponse {
 
     private Long id;
     private String name;
@@ -19,8 +16,8 @@ public class ProductListResponse implements Serializable {
     private String mainFile;
     private int price;
     private double discountRate;
-    private int discountedPrice;
 
+    @Builder
     @QueryProjection
     public ProductListResponse(Long id, String name, String brand, String mainFile, int price, double discountRate) {
         this.id = id;
@@ -28,29 +25,6 @@ public class ProductListResponse implements Serializable {
         this.brand = brand;
         this.mainFile = mainFile;
         this.price = price;
-        this.discountRate = (int) discountRate;
-    }
-
-    @Builder
-    public ProductListResponse(Long id, String name, String brand, String mainFile, int price, double discountRate, int discountedPrice) {
-        this.id = id;
-        this.name = name;
-        this.brand = brand;
-        this.mainFile = mainFile;
-        this.price = price;
         this.discountRate = discountRate;
-        this.discountedPrice = discountedPrice;
-    }
-
-    public static ProductListResponse of(Product product) {
-        return ProductListResponse.builder()
-                .id(product.getId())
-                .name(product.getTitle())
-                .brand(product.getProductOption().getBrand().getName())
-                .mainFile(product.getProductOption().getFile().getFileName())
-                .price(product.getProductOption().getPrice())
-                .discountRate(product.getDiscountRate())
-                .discountedPrice(product.getProductOption().getPrice() - (int) (product.getProductOption().getPrice() * (product.getDiscountRate() / 100)))
-                .build();
     }
 }
